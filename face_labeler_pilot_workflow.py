0052fd51af5dcecc6b03c8266f89115478720727
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2024-05-15 20:18:20 -0700

    Update face_labeler_pilot_workflow.py
    Further introduced the Face class throughout the workflow.

--- face_labeler_pilot_workflow.py
+++ face_labeler_pilot_workflow.py
@@ -16,23 +16,18 @@ import textwrap
 
 class Face:
     # structure to store information about detected faces
-    def __init__(self):
-        self.img_path = ""
-        self.img_height = 0
-        self.img_width = 0
+    def __init__(self, img_path, img_height, img_width, face_location, encoding):
+        self.img_path = img_path
+        self.img_height = img_height
+        self.img_width = img_width
+        self.face_location = face_location
         self.face_top = 0
         self.face_right = 0
         self.face_bottom = 0
         self.face_left = 0
-        self.encoding = []
+        self.encoding = encoding
         self.match_candidate = True
-        self.person_in_image = ""
-
-    def face_location(self):
-        return tuple([self.face_top, self.face_right, self.face_bottom, self.face_left])
-
-    def current_face(self):
-        return self.img_path, self.face_location(), self.encoding, self.match_candidate
+        self.person_shown = ""
 
     def open_image(self):
         img = cv2.imread(self.img_path)
@@ -49,7 +44,7 @@ def strip_faces(img_paths):
     _status_bar = st.progress(0, 'Firing up the face detection algorithm!')
     time.sleep(1)
     # keep a queue of found faces
-    # queue is a list of tuples (image path, face bounding box, face encoding)
+    # queue is a list of instances of class Face
     q = deque()
     # iterate over all image paths is the selected directory and gather all detected faces and face encodings
     for i in range(len(img_paths)):
@@ -65,21 +60,18 @@ def strip_faces(img_paths):
         # detect face locations in image
         face_locations = face_recognition.face_locations(resized_image, model='hog')
         for face_location in face_locations:
-            _top, _right, _bottom, _left = face_location
-            # crop the image to the face
-            # face_image = resized_image[top:bottom, left:right]
             # get face encoding
             encodings = face_recognition.face_encodings(resized_image,
                                                         known_face_locations=[face_location],
                                                         num_jitters=1,
                                                         model="large")
-            face = Face()
-            face.img_path = img_paths[i]
-            face.face_top, face.face_right, face.face_bottom, face.face_left = [d * 4 for d in face_location]
-            face.encoding = encodings
-
-            # update the queue
-            q.append(face)
+            # update the queue with a new instance of class Face
+            q.append(Face(img_path=img_paths[i],
+                          img_height = image.shape[0],
+                          img_width = image.shape[1],
+                          face_location=tuple([d * 4 for d in face_location]),
+                          encoding=encodings)
+                     )
 
     _status_bar.empty()
 
@@ -88,21 +80,25 @@ def strip_faces(img_paths):
 
 def record_name():
     if sess.selected_name:
+        _current_face = sess['faces_detected'][0]
+        # if the user selected 'Someone else', that means the face recognition algorithm
+        # predicted the name belonging to the face, but the user disagrees with the prediction
+        # and wants to correct the name with a name that is not in the current list of names.
+        # We mark the face's match candidate attribute False so we can revisit this face and
+        # enter a new name to the list. In doing this we provide the user with a new selectbox (free_text_select).
         if sess.selected_name == 'Someone else':
-            sess['faces_detected'][0].match_candidate = False
+            _current_face.match_candidate = False
         else:
             if sess.selected_name == 'Not a face':
                 sess.faces_count -= 1
             else:
-                sess.name_options[sess.selected_name] += 1
+                _current_face.person_shown = sess.selected_name
+                sess.name_options[_current_face.person_shown] += 1
                 sess.face_i += 1
-                if sess['faces_detected'][0].img_path not in sess.labeled:
-                    sess.labeled[sess['faces_detected'][0].img_path] = {'face_locations': [], 'person_shown': []}
-                sess.labeled[sess['faces_detected'][0].img_path]['face_locations'].append(sess['faces_detected'][0].face_location())
-                sess.labeled[sess['faces_detected'][0].img_path]['person_shown'].append(sess.selected_name)
-                if len(sess['faces_detected'][0].encoding) > 0:
-                    sess.data['encodings'].append(sess['faces_detected'][0].encoding[0])
-                    sess.data['names'].append(sess.selected_name)
+                sess.labeled[_current_face.img_path].append(_current_face)
+                if len(_current_face.encoding) > 0:
+                    sess.data['encodings'].append(_current_face.encoding[0])
+                    sess.data['names'].append(_current_face.person_shown)
             sess['faces_detected'].popleft()
 
     return
@@ -134,7 +130,7 @@ if detect_faces and select_folder:
     # count of faces labeled
     sess['face_i'] = 1
     # dictionary of labeled faces
-    sess['labeled'] = {}
+    sess['labeled'] = defaultdict(list)
     # dictionary of face encodings and names
     sess['data'] = {'encodings': [], 'names': []}
     # dictionary of names/identities and counts
@@ -157,20 +153,17 @@ if 'faces_detected' in sess:
                                  text=f'Labeling face {sess.face_i} of {sess.faces_count}')
         # pop the next face from the queue
         current_face = sess['faces_detected'][0]
-        # print(current_face.current_face())
-        sess.img_path, sess.face_location, sess.encoding, sess.match = current_face.current_face()
-
-        img = cv2.imread(sess.img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # img_H, img_W, _ = img.shape
-        top, right, bottom, left = sess.face_location
-        img = img[top:bottom, left:right]
+        # open cropped image of current face
+        current_face_img = cv2.imread(current_face.img_path)
+        current_face_img = cv2.cvtColor(current_face_img, cv2.COLOR_BGR2RGB)
+        top, right, bottom, left = current_face.face_location
+        current_face_img = current_face_img[top:bottom, left:right]
         # check if the current face has an encoding
-        if len(sess.encoding) > 0:
+        if len(current_face.encoding) > 0:
             # compare the face encoding to existing encodings to see if we can find a match
             # note: the lower the tolerance, the more sensitive the algorithm is at matching faces
             matches = face_recognition.compare_faces(sess.data['encodings'],
-                                                     sess.encoding[0],
+                                                     current_face.encoding[0],
                                                      tolerance=0.55)
             # st.write(matches)
 
@@ -184,32 +177,30 @@ if 'faces_detected' in sess:
                 # st.write(list(sess.name_options.keys()))
                 if not auto_confirm_matches:
                     with st.form(str(uuid.uuid4())):
-                        st.image(img, width=100)
-                        if sess.match:
+                        st.image(current_face_img, width=100)
+                        if current_face.match_candidate:
                             st.write(f'I think this face belongs to **{predicted_name}**, can you confirm?')
-                            selected_name = st.selectbox(label=('The predicted name has been pre-selected, '
-                                                                'click the submit button to confirm.\n\n'
-                                                                'Select "Not a face" to skip this face.\n\n'
-                                                                'Select "Someone else" if their name '
-                                                                'is not in the list.\n'
-                                                                ),
-                                                         options=['Not a face', 'Someone else'] + sorted(
-                                                             sess.name_options.keys()),
-                                                         index=sorted(sess.name_options.keys()).index(
-                                                             predicted_name) + 2,
-                                                         key='selected_name')
-                        elif not sess.match:
+                            st.selectbox(label=('The predicted name has been pre-selected, '
+                                                'click the submit button to confirm.\n\n'
+                                                'Select "Not a face" to skip this face.\n\n'
+                                                'Select "Someone else" if their name '
+                                                'is not in the list.\n'
+                                                ),
+                                         options=['Not a face', 'Someone else'] + sorted(sess.name_options.keys()),
+                                         index=sorted(sess.name_options.keys()).index(predicted_name) + 2,
+                                         key='selected_name')
+                        elif not current_face.match_candidate:
                             st.write((f"I think this face belongs to **{predicted_name}**, "
                                       "but you think it's someone else, who do you think this is?"))
-                            selected_name = st_free_text_select(label=('Who do you think this is? Type in a new name '
-                                                                       'or select one from the list. '
-                                                                       'Select "Not a face" to skip this face.'),
-                                                                options=sorted(sess.name_options.keys()),
-                                                                key="selected_name"
-                                                                )
+                            st_free_text_select(label=('Who do you think this is? Type in a new name '
+                                                       'or select one from the list. '
+                                                       'Select "Not a face" to skip this face.'),
+                                                options=sorted(sess.name_options.keys()),
+                                                key="selected_name"
+                                                )
                         st.form_submit_button(label='Submit', on_click=record_name)
                 elif auto_confirm_matches:
-                    st.image(img, width=100)
+                    st.image(current_face_img, width=100)
                     st.write(f"This face belongs to **{predicted_name}**")
                     sess['selected_name'] = predicted_name
                     # wait for a moment, user can still interrupt by unchecking auto confirm matches
@@ -219,28 +210,26 @@ if 'faces_detected' in sess:
 
             else:
                 with st.form(str(uuid.uuid4())):
-                    st.image(img, width=100)
+                    st.image(current_face_img, width=100)
                     st.write("I don't recognize this face, who is this?")
-                    selected_name = st_free_text_select(label=('Type in a new name or select one from the list. '
-                                                               'Select "Not a face" to skip this face.'),
-                                                        options=['Not a face'] + sorted(
-                                                            sess.name_options.keys()),
-                                                        key="selected_name"
-                                                        )
+                    st_free_text_select(label=('Type in a new name or select one from the list. '
+                                               'Select "Not a face" to skip this face.'),
+                                        options=['Not a face'] + sorted(sess.name_options.keys()),
+                                        key="selected_name"
+                                        )
                     st.form_submit_button(label='Submit', on_click=record_name)
-        else:
+        elif not current_face.encoding:
             with st.form(str(uuid.uuid4())):
-                st.image(img, width=100)
+                st.image(current_face_img, width=100)
                 st.write('This face has no encoding. Is this a face?')
-                selected_name = st_free_text_select(label=('Type in a new name or select one from the list. '
-                                                           'Select "Not a face" to skip this face.'),
-                                                    options=['Not a face'] + sorted(
-                                                        sess.name_options.keys()),
-                                                    key="selected_name"
-                                                    )
+                st_free_text_select(label=('Type in a new name or select one from the list. '
+                                           'Select "Not a face" to skip this face.'),
+                                    options=['Not a face'] + sorted(sess.name_options.keys()),
+                                    key="selected_name"
+                                    )
                 st.form_submit_button(label='Continue', on_click=record_name)
     # if our queue of faces is empty, check if we have labeled any images
-    else:
+    if not sess['faces_detected']:
         success_text = "All faces have been labeled!"
         st.success(success_text, icon='✅')
         df = pd.DataFrame(data=sess.name_options.items(),
@@ -249,24 +238,18 @@ if 'faces_detected' in sess:
         st.dataframe(df.sort_index())
         # if we have labeled data, let's embed the face locations and names in the image metadata
         if 'labeled' in sess:
-            st.write(sess.labeled)
-            # st.write(sess.data)
             write_metadata = st.button(label="Write Metadata")
             if write_metadata:
                 status_text = 'Begin writing metadata to files!'
                 status_bar = st.progress(0, status_text)
                 n = len(sess.labeled)
                 j = 0
-                for image_path, image_data in sess.labeled.items():
+                for image_path, faces in sess.labeled.items():
                     status_bar.progress((j + 1) / n,
                                         text=f'({j + 1} of {n}) Writing metadata to {image_path.split("/")[-1]}...')
-                    img = cv2.imread(image_path)
-                    img_h, img_w = img.shape[:2]
-                    # with exiftool.ExifToolHelper() as et:
-                    # et.execute("-XMP-mwg-rs:RegionInfo=", image_path)
-                    # metadata = et.get_metadata(image_path)
-                    for i, person_shown in enumerate(image_data['person_shown']):
-                        top, right, bottom, left = image_data['face_locations'][i]
+                    for i, face in enumerate(faces):
+                        top, right, bottom, left = face.face_location
+                        img_h, img_w = face.img_height, face.img_width
                         W = round((right - left) / img_h, 4)
                         H = round((bottom - top) / img_w, 4)
                         X = round(left / img_h, 4)
@@ -277,10 +260,10 @@ if 'faces_detected' in sess:
                             # st.write(tags)
                             if "XMP:PersonInImage" not in tags:
                                 # st.write(f'Setting PersonInImage to {person_shown}')
-                                et.execute(f"-XMP:PersonInImage={person_shown}", image_path)
-                            elif person_shown not in tags["XMP:PersonInImage"]:
+                                et.execute(f"-XMP:PersonInImage={face.person_shown}", image_path)
+                            elif face.person_shown not in tags["XMP:PersonInImage"]:
                                 # st.write(f'Appending {person_shown} to PersonInImage')
-                                et.execute(f"-XMP:PersonInImage+={person_shown}", image_path)
+                                et.execute(f"-XMP:PersonInImage+={face.person_shown}", image_path)
 
                             if "XMP:RegionName" not in tags:
                                 # st.write(f'Setting RegionName to {person_shown}')
@@ -289,16 +272,16 @@ if 'faces_detected' in sess:
                                                        "Unit=pixel}, RegionList=[{Area={"
                                                        f"W={W}, H={H}, X={X}, Y={Y},"
                                                        "Unit=normalized}, "
-                                                       f"Name={person_shown},"
+                                                       f"Name={face.person_shown},"
                                                        "Type=Face}]}")
                                 print(execution_string)
                                 et.execute(execution_string, image_path)
-                            elif person_shown not in tags["XMP:RegionName"]:
+                            elif face.person_shown not in tags["XMP:RegionName"]:
                                 # st.write(f'Appending {person_shown} to RegionName')
                                 execution_string = str("-XMP-mwg-rs:RegionList+=[{Area={"
                                                        f"W={W}, H={H}, X={X}, Y={Y},"
                                                        "Unit=normalized}, "
-                                                       f"Name={person_shown},"
+                                                       f"Name={face.person_shown},"
                                                        "Type=Face}]}")
                                 print(execution_string)
                                 et.execute(execution_string, image_path)
