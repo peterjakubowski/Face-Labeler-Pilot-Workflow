9239c8127e03ec3da61faab4b351cb40a0da9baf
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-22 17:25:47 -0400

    Create helpers.py

---
+++ utils/helpers.py
@@ -0,0 +1,81 @@
+import streamlit as st
+from pathlib import Path
+from imutils import paths
+from config import IMG_DIR
+import os
+from utils.image_processing import detect_faces
+from config import IMG_SIZE
+from collections import defaultdict
+
+
+def list_folders_in_watch_folder() -> list[str]:
+    # Make the 'watch_folder' directory if it does not exist
+    Path.mkdir(IMG_DIR, exist_ok=True)
+    # List the subfolders of the 'watch_folder'
+    folder_names = [d for d in os.listdir(IMG_DIR) if os.path.isdir(os.path.join(IMG_DIR, d))]
+
+    return folder_names
+
+
+def run_face_detection_workflow(select_folder: str):
+    # list all the images (paths) in the selected folder
+    st.session_state['image_paths'] = sorted(paths.list_images(os.path.join(IMG_DIR, select_folder)),
+                                 key=lambda x: x.split('/')[-1])
+    # detect faces in all the images, get a list/queue of faces (instances of Face class)
+    st.session_state['faces_detected'] = detect_faces(img_paths=st.session_state.image_paths, img_size=IMG_SIZE)
+    # count how many faces were detected
+    st.session_state['faces_count'] = len(st.session_state.faces_detected)
+    # if we didn't detect any faces, delete the queue from the session state and display a message
+    if st.session_state.faces_count < 1:
+        del st.session_state['faces_detected']
+        st.warning(f"Workflow complete! "
+                   f"Looked for faces in {len(st.session_state['image_paths'])} images "
+                   f"and {st.session_state['faces_count']} faces were found.")
+    # count of faces labeled
+    st.session_state['face_i'] = 1
+    # dictionary of labeled faces
+    st.session_state['labeled'] = defaultdict(list)
+    # dictionary of face encodings and names
+    st.session_state['data'] = {'encodings': [], 'names': []}
+    # dictionary of names/identities and counts
+    st.session_state['name_options'] = defaultdict(int)
+
+
+def record_name() -> None:
+    """
+    Function for recording a name for a labeled person.
+    Updates the current face class with modifications.
+    If the current face is labeled, then it is added to
+    the dictionary of labeled faces and removed from
+    the queue of faces to label.
+    :return: None
+    """
+
+    if st.session_state.selected_name:
+        # peek at the first face in the queue of detected faces
+        current_face = st.session_state['faces_detected'][0]
+        # if st.session_state.selected_name == 'Someone else':
+        #     current_face.match_candidate = False
+        # else:
+        if st.session_state.selected_name == 'Not a face':
+            # decrement the count of detected faces
+            st.session_state.faces_count -= 1
+        else:
+            # update the current face's person shown attribute with the selected name
+            current_face.person_shown = st.session_state.selected_name
+            # increment the count for the number of times faces have been labeled with this name
+            st.session_state.name_options[current_face.person_shown] += 1
+            st.session_state.face_i += 1
+            # add the current face to the dictionary of labeled faces
+            st.session_state.labeled[current_face.img_path].append(current_face)
+            # if the current face has an encoding, append it along with the name
+            # to the list of encodings and names for future face recognitions
+            if len(current_face.encoding) > 0:
+                st.session_state.data['encodings'].append(current_face.encoding[0])
+                st.session_state.data['names'].append(current_face.person_shown)
+        # pop the current face from the queue
+        st.session_state['faces_detected'].popleft()
+
+        st.session_state['selected_name'] = None
+
+    return
