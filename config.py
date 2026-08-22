71d370ccf97bc888e849054a19286c9ea88b1745
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-22 17:25:59 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+IMG_SIZE = 1024  # Set the image size for inference, the number of pixels the longest edge should be resized to
+IMG_DIR = Path("watch_folder")  # Set the path to the 'watch_folder' directory
+COMPARE_FACES_TOLERANCE = 0.55  # the lower the tolerance, the more sensitive the algorithm is at matching faces
