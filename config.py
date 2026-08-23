d055481d739c0bd6dcede15d3e557baf4cc4b87b
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-08-23 13:23:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ IMG_SIZE = 1024  # Set the image size for inference, the number of pixels the lo
 IMG_DIR = Path("watch_folder")  # Set the path to the 'watch_folder' directory
 COMPARE_FACES_TOLERANCE = 0.55  # the lower the tolerance, the more sensitive the algorithm is at matching faces
 AUTO_CONFIRM_MATCHES_TIME = 1  # Number of seconds to wait before submitting predicted name
+IMG_PREVIEW_WIDTH = 1500
