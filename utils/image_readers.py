fb256345b8d626bf23438bc30535f53ac4a98f85
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-08 18:47:21 -0400

    Update image_readers.py

--- utils/image_readers.py
+++ utils/image_readers.py
@@ -1,10 +1,8 @@
-import io
 from pathlib import Path
 
 import cv2
 import numpy as np
 import rawpy
-from PIL import Image
 
 
 def read_image_with_cv2(image_path: Path) -> np.array:
@@ -24,10 +22,9 @@ def convert_thumbnail_to_array(thumbnail: rawpy.Thumbnail) -> np.array:
     :return: Thumbnail image as a numpy array
     """
 
-    thumbnail_bytes = io.BytesIO(thumbnail.data)
-    thumbnail_image = Image.open(thumbnail_bytes)
+    thumbnail_image = np.frombuffer(thumbnail.data, dtype=np.uint8)
 
-    return cv2.cvtColor(np.array(thumbnail_image), cv2.COLOR_RGB2BGR)
+    return cv2.imdecode(thumbnail_image, cv2.IMREAD_COLOR)
 
 
 def read_image_with_rawpy(image_path: Path) -> np.array:
