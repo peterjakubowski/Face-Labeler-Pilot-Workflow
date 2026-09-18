e547d618b59065d27cb8e3080fc332969fd161af
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-18 15:22:11 -0400

    Update exiftool.py

--- utils/exiftool.py
+++ utils/exiftool.py
@@ -63,6 +63,7 @@ def write_metadata_with_exiftool():
     time.sleep(1)
     n = len(st.session_state.labeled)
     for j, (image_path, faces) in enumerate(st.session_state.labeled.items()):
+        image_path = Path(image_path)
         # update our progress
         status_bar.progress(
             (j + 1) / n,
