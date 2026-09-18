5b73d3ec990e136b632e828b3e32f224068993bd
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-18 15:08:58 -0400

    Update app.py
    Add new settings page to navigation.

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ workflow = st.Page(page="pages/workflow.py", title="Workflow")
 
 viewer = st.Page(page="pages/viewer.py", title="Image Viewer")
 
-pages = st.navigation([workflow, viewer])
+settings = st.Page(page="pages/settings.py", title="Settings")
+
+pages = st.navigation([workflow, viewer, settings])
 
 pages.run()
