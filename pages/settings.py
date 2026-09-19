3894fc7cdf10769e6e0f8db28b56b83bcf13b112
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-19 13:10:15 -0400

    Update settings.py
    Rename page to "Face recognition settings" from "Face classifier settings". Changed element to st.title from st.header.

--- pages/settings.py
+++ pages/settings.py
@@ -4,7 +4,7 @@ from config import COMPARE_FACES_TOLERANCE, TOP_K
 from utils.face_classifier import face_conn
 from utils.library import prepare_reference_data
 
-st.header("Face classifier settings")
+st.title("Face recognition settings")
 
 st.write(st.session_state)
 
