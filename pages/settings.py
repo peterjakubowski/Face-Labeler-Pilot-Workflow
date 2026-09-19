1a135b1cacca68bd4636b75dd9e86d9a54d31e70
peterjakubowski <97050386+peterjakubowski@users.noreply.github.com>
2026-09-19 13:40:30 -0400

    Update settings.py

--- pages/settings.py
+++ pages/settings.py
@@ -4,7 +4,7 @@ from config import COMPARE_FACES_TOLERANCE, TOP_K
 from utils.face_classifier import face_conn
 from utils.library import prepare_reference_data
 
-st.title("Face recognition settings")
+st.title("Face Recognition Settings")
 
 st.write(st.session_state)
 
